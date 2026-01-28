7b068726c908bd3b6d80aa54b347a3659120b9fd
class12 <mansoorihassan409@gmail.com>
2026-01-28 10:36:45 +0500

    todo-app completed

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ var todos =  JSON.parse(localStorage.getItem('todos')) || [];
 
 
 function todo() {
-    // if(inp.value.trim() !== '') return
+    if(input.value.trim() === '') return
  
-    console.log('input.value' , input.value);
+    // console.log('input.value' , input.value);
 
     var obj = {
         id: Date.now(),
         text: input.value,
-        status: false,
+        completed: false,
 
     }
 
@@ -22,20 +22,103 @@ function todo() {
 
     localStorage.setItem('todos' , JSON.stringify(todos));
 
-    rendetTodo()    
+    renderTodo()    
     
 }
 
-function rendetTodo(){
-    // ul.innerHTML = "";
+function renderTodo(){
+    ul.innerHTML = "";
     for (let i = 0; i < todos.length; i++) {
+        let id = todos[i].id;
+        // console.log(id);
+        
         var li = document.createElement('li');
         li.innerText = todos[i].text;
         ul.appendChild(li);
         
+
+
+
+        // delete btn
+        var dlt = document.createElement('button');
+        dlt.innerHTML = 'Delete';
+        li.appendChild(dlt);
+        dlt.addEventListener('click' , function () {
+            deleteTodos(id);
+        })
+
+
+
+        // edit btn
+
+        var edit = document.createElement('button');
+        edit.innerHTML = 'Edit';
+        li.appendChild(edit);
+        edit.addEventListener('click' , function () {
+            editTodos(id);
+        })
+
+        // status btn
+        var status = document.createElement('button');
+        if (todos[i].completed === true) {
+            status.innerHTML = 'Completed';
+        }
+        else{
+            status.innerHTML = 'Pending';
+        }
+        li.appendChild(status);
+
+        status.addEventListener('click' , function () {
+            changeStatus(id);
+        })
     }
 
 }
+
+function deleteTodos(id) {
+   var newTodos = [];
+   for(let i = 0; i < todos.length; i++){
+    if(todos[i].id !== id){
+       newTodos.push(todos[i])     
+    }
+   }
+   todos = newTodos;
+   localStorage.setItem('todos', JSON.stringify(todos));
+   
+   renderTodo();
+   
+}
+
+function editTodos(id) {
+    for(let i = 0; i < todos.length; i++){
+        if(todos[i].id === id){
+            let update = prompt('Edit', todos[i].text);
+            todos[i].text = update;
+            localStorage.setItem('todos', JSON.stringify(todos));
+        }
+    }
+    renderTodo()
+}
+
+function changeStatus(id) {
+    for (let i = 0; i < todos.length; i++) {
+        if (todos[i].id === id) {
+            if (todos[i].completed === false) {
+                todos[i].completed = true
+            }
+            else{
+                todos[i].completed = false;
+            }
+        }
+        
+    }
+      localStorage.setItem('todos', JSON.stringify(todos));
+     renderTodo()
+    
+}
+
+
+renderTodo();
 
 
 
